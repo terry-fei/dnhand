dc053ad1c72ea217229259b19c1ce2938c2d882d
ifeiteng <ifeiteng@gmail.com>
2014-12-15 17:55:18 +0800

    use login local

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,12 @@
 FROM docker.cn/docker/node:latest
 
+RUN apt-get install -y python-imaging
+
 RUN \
   npm install -g coffee-script && \
-  npm install -g pm2
+  npm install -g pm2 
 
+ENV NODE_ENV production
 ENV MONGO_HOST mongo
 ENV MONGO_PORT 27017
 ENV MONGO_DBNAME dnhand
---
+++ lib/JwcLoginHelper.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+import urllib
+import urllib2
+from PIL import Image
+import io
+from fonts import code, codeCount, codeWidth, codePrefix, codeHash, codeLocPrefix
+import json
+
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+class JwcLoginHelper:
+
+  indexUrl  = "http://202.118.167.86:9003"
+  codeUrl   = "http://202.118.167.86/validateCodeAction.do"
+  verifyUrl = "http://202.118.167.86/loginAction.do"
+  indexUrl1 = "http://202.118.167.85:9004/"
+  indexUrl2 = "http://202.118.167.86:9002/"
+  indexUrl3 = "http://202.118.167.86:9001/"
+
+  def __init__(self, stuid, pswd):
+    self.stuid = stuid
+    self.pswd  = pswd
+
+  #  0 -- success  1 -- connectError  2 -- identifyError  3 -- codeError   
+  def login(self):
+    self._getCookieAndCodeImg()
+    if self.getCookieSuccess != True:
+      return {'errcode': 1, 'errmsg': 'connect error'}
+    self._identifyCode()
+    self._verifyAccount()
+    return self.loginResult
+
+  def _verifyAccount(self):
+    params = {"dzslh": "", "eflag": "", "evalue": "", "fs": "", "lx": "", "tips": "", "zjh1": "", 
+      "mm": self.pswd, "v_yzm": self.code, "zjh": self.stuid }
+    headers = {'Cookie': self.cookie}
+    data = urllib.urlencode(params)
+    req = urllib2.Request(self.verifyUrl, data, headers)
+    try:
+      res = urllib2.urlopen(req)
+    except:
+      pass
+    else:
+      result = res.read()
+      if "frameset" in result:
+        self.loginResult = {'errcode': 0, 'ticket': self.cookieValue}
+        return
+      result = result.decode('gb2312')
+      if "你输入的证件号不存在" in result:
+        self.loginResult = {'errcode': 2, 'errmsg': 'username or password wrong'}
+      elif "你输入的验证码错误" in result:
+        self.loginResult = {'errcode': 3, 'errmsg': 'code wrong'}
+      else:
+        self.loginResult = {'errcode': 4, 'errmsg': 'unknow wrong'}
+
+  def _getCookieAndCodeImg(self):
+    try:
+      res = urllib2.urlopen(url=self.codeUrl, timeout=3)
+      if res.getcode() == 200:
+        self.cookie = res.info()['Set-Cookie']
+      else:
+        return
+    except:
+      self.getCookieSuccess = False
+    else:
+      self.getCookieSuccess = True
+      self.cookieValue = self.cookie[11:32]
+      self.codeImg = Image.open(io.BytesIO(res.read()))
+
+  def _identifyCode(self):
+    ip = self.codeImg.convert("L").load()
+    ps = [[0 for w in range(60)] for h in range(20)]
+    for h in range(20):
+      for w in range(60):
+        if ip[w,h] <= 128:
+          ps[h][w]=1
+    codeArr = []
+    for index in range(1, 5):
+      char = self._getCodeChar(ps, str(index))
+      codeArr.append(char)
+    codeStr = ""
+    for item in codeArr:
+      codeStr += item
+    self.code = codeStr
+
+  def _getCodeChar(self, ps, index):
+    prefix = codeLocPrefix[index]
+    chars = {}
+    for charNum in range(62):
+      char = str(charNum)
+      hitTimes = 0
+      for r in range(20):
+        for c in range(codeWidth[char]):
+          if code[char][r][c] != 0 and ps[r][c+prefix+codePrefix[char]] != 0:
+            hitTimes += 1
+      if codeCount[char] != 0:
+        hitRate = hitTimes/codeCount[char]
+        if hitRate != 1.0:
+          chars[char] = hitRate
+        else:
+          return codeHash[char]
+
+    maxHitRate = 0.0
+    hitChar = ""
+    for k, v in chars.items():
+      if v > maxHitRate:
+        maxHitRate = v
+        hitChar = k
+    return codeHash[hitChar]
+
+if __name__ == '__main__':
+  loginHelper = JwcLoginHelper("A19120626", "1230.0")
+  print(loginHelper.login())
