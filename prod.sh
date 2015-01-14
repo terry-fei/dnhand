34fb5f9b2295ba3523a36f9ea5762dffe390834a
ifeiteng <ifeiteng@gmail.com>
2015-01-14 16:25:20 +0000

    restart

--- prod.sh
+++ prod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run -d --name dnhand -v $(pwd):/src -p 7080:80 --link mongodb:mongo --link redis:redis \
+docker run -d --name dnhand -v $(pwd):/src --link mongodb:mongo --link redis:redis \
   dnhand pm2 start boot.json --no-daemon
