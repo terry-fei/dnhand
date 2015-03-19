5102162aafeb3f9060402ace2c04d3c340630604
ifeiteng <ifeiteng@gmail.com>
2015-03-19 18:32:38 +0800

    fix boot config

--- dev.sh
+++ dev.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker run -it --rm -v $(pwd):/usr/src --link mongo:mongo --link redis:redis \
-  -e VIRTUAL_HOST=test.feit.me \
+  -e VIRTUAL_HOST=dnhandtmp.feit.me \
   node:0.10 bash
